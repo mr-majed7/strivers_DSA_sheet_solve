ddb763f24cff21e66bcc0fd3018c18ece0ee7397
majed <majedulislam7789@gmail.com>
2024-07-02 20:28:55 +0600

    1 Linked List

---
+++ linked_list/medium_ll/add_two_numbers.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/add-two-numbers/
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        dummy = ListNode()
+        temp = dummy
+        carry = 0
+
+        while l1 is not None or l2 is not None or carry:
+            sum = 0
+
+            if l1 is not None:
+                sum += l1.val
+                l1 = l1.next
+
+            if l2 is not None:
+                sum += l2.val
+                l2 = l2.next
+
+            sum += carry
+            carry = sum // 10
+
+            newNode = ListNode(sum % 10)
+            temp.next = newNode
+            temp = temp.next
+        return dummy.next
